b02110cb507af7b343bbf6a07e214fb5f9123ed3
Hirwa Arnold <hategekimanahirwaarnold@gmail.com>
2023-07-24 02:10:35 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const multer = require('multer');
 
 var app = express();
-
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +13,30 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    //define destination folder where files will be stored
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    //define how files will be named.
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
 
+const upload = multer({
+  storage: storage,
+})
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=> {
+  if (!req.file) {
+    return res.status(400).json({error: "No file uploaded!"});
+  };
+  //if file uploaded, access the file's details
+   console.log('File uploaded successfully:', req.file);
+   let received = req.file;
+  res.status(200).json({ name: received.originalname, type: received.mimetype, size: received.size});
+})
 
 
 const port = process.env.PORT || 3000;
